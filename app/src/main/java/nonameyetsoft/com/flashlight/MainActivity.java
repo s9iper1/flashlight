cb39750768dca52ba711cf794b558bd83f303b7c
muhammadimran2 <m07ansari@gmail.com>
2022-02-08 20:09:37 +0500

    Update

--- app/src/main/java/nonameyetsoft/com/flashlight/MainActivity.java
+++ app/src/main/java/nonameyetsoft/com/flashlight/MainActivity.java
@@ -1,12 +1,12 @@
 package nonameyetsoft.com.flashlight;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
