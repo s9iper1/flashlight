088018641c67781495013d316be14f035ccae0cc
muhammadimran2 <m07ansari@gmail.com>
2022-02-08 21:08:07 +0500

    Torch On/off

--- app/src/main/java/nonameyetsoft/com/flashlight/MainActivity.java
+++ app/src/main/java/nonameyetsoft/com/flashlight/MainActivity.java
@@ -1,39 +1,86 @@
 package nonameyetsoft.com.flashlight;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView torch;
+    private boolean state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        torch = findViewById(R.id.torch);
+
+        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        torchLight();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(getApplicationContext(), "Camera Permission Required ", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest,
+                                                                   PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+
+                    }
+                }).check();
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void torchLight() {
+        torch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!state) {
+                    CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                    try {
+                        String cameraID = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraID, true);
+                        state = true;
+                        torch.setImageResource(R.drawable.torchon);
+                    } catch (Exception exception) {
+                        Toast.makeText(getApplicationContext(), "" + exception, Toast.LENGTH_SHORT).show();
+                    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                } else {
+                    CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
-        return super.onOptionsItemSelected(item);
+                    try {
+                        String cameraID = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraID, false);
+                        state = false;
+                        torch.setImageResource(R.drawable.torchoff);
+                    } catch (Exception exception) {
+                        Toast.makeText(getApplicationContext(), "" + exception, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
 }
